113817ea2d2a795cafad8ddb2db8cdadbdf1b895
RiyaAtta <84119983+RiyaAtta@users.noreply.github.com>
2021-06-02 01:31:50 +0530

    Delete src/main/java/com/capgemini/cmapp directory

--- src/main/java/com/capgemini/cmapp/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.cmapp.service;
-
-//interface consisting of the CRUD methods
-public interface BookService {
-	void create();
-	void read();
-	void update();
-	void delete();
-}
