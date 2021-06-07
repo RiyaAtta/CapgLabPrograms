bfbe8da0370382188da0e8e24404b98ece4df408
RiyaAtta <84119983+RiyaAtta@users.noreply.github.com>
2021-06-07 17:57:37 +0530

    Add files via upload
    committing day 15

---
+++ src/Day15/com/capgemini/concurrency/executorexample/RunnableOne.java
@@ -0,0 +1,16 @@
+package Day15.com.capgemini.concurrency.executorexample;
+
+public class RunnableOne implements Runnable{
+
+	@Override
+	public void run() {
+		while(true) {
+			System.out.println("Executing the task one");
+			try {
+				Thread.sleep(1000);
+			}catch(Throwable e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
